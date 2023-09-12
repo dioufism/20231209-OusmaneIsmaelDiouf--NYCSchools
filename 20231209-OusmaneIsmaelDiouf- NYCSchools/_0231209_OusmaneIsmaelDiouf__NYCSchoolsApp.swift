0dd65f1602fdd1190b56a373eeb55cc66a69213a
Ousmane <dioufism@gmail.com>
2023-09-12 13:51:38 -0500

    Initial Commit

---
+++ 20231209-OusmaneIsmaelDiouf- NYCSchools/_0231209_OusmaneIsmaelDiouf__NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0231209_OusmaneIsmaelDiouf__NYCSchoolsApp.swift
+//  20231209-OusmaneIsmaelDiouf- NYCSchools
+//
+//  Created by ousmane diouf on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct _0231209_OusmaneIsmaelDiouf__NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
